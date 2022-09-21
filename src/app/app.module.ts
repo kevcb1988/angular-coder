32decf9398b0c1323982432093421e05ce7a0629
Keven CB <kevcb1988@gmail.com>
2022-09-20 20:14:12 -0500

    Inicio de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+import { ContenidoPrincipalComponent } from './components/contenido-principal/contenido-principal.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    ContenidoPrincipalComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/contenido-principal/contenido-principal.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContenidoPrincipalComponent } from './contenido-principal.component';
+
+describe('ContenidoPrincipalComponent', () => {
+  let component: ContenidoPrincipalComponent;
+  let fixture: ComponentFixture<ContenidoPrincipalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContenidoPrincipalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContenidoPrincipalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
